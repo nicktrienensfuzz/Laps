ec35fedeb0a0d4c38dc6b81ac7385b871dccbce2
Nicholas Trienens <nick.trienens@monstar-lab.com>
2022-06-20 22:25:45 -0600

    Initial Commit

---
+++ Laps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Laps
+//
+//  Created by Nicholas Trienens on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Laps/LapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LapsApp.swift
+//  Laps
+//
+//  Created by Nicholas Trienens on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
